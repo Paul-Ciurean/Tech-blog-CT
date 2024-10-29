663690368d44f647ded42a6bd863f293df52577b
Ciprian Paul Ciurean <paul_0307@yahoo.com>
2024-10-29 20:28:57 +0000

    Blog page

--- main.tf
+++ main.tf
@@ -23,9 +23,10 @@ provider "aws" {
 # Variables #
 #############
 
-variable "aws_account_id" {
-  description = "Your Account ID"
-  type        = string
+variable "domain_name" {
+  description = "The domain name of my blog"
+  type = string
+  default = "paul-test-projects.co.uk"
 }
 
 ###############################################
@@ -87,38 +88,81 @@ data "aws_iam_policy_document" "allow_public_access" {
   }
 }
 
-##########################################
-# Cloud Front distribution configuration #
-##########################################
+###############################
+# R53 and Certificate Manager #
+###############################
+
+resource "aws_route53_zone" "zone_p10" {
+  name = var.domain_name
+}
+
+resource "aws_acm_certificate" "cert_p10" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  provider = aws.n-virginia
+
+  subject_alternative_names = [
+    "www.${var.domain_name}"
+  ]
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert_p10.domain_validation_options : dvo.domain_name => dvo
+  }
+
+  zone_id = aws_route53_zone.zone_p10.zone_id
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  ttl     = 60
+  records = [each.value.resource_record_value]
+}
+
+resource "aws_acm_certificate_validation" "cert_validation_p10" {
+  certificate_arn = aws_acm_certificate.cert_p10.arn
+  provider = aws.n-virginia
+
+  validation_record_fqdns = [
+    for record in aws_route53_record.cert_validation : record.fqdn
+  ]
+
+   depends_on = [aws_route53_record.cert_validation]
+}
+
+
+##############
+# CloudFront #
+##############
 
 locals {
   s3_origin_id = "myS3Origin"
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket_website_configuration.static_website.website_endpoint
-    origin_id                = local.s3_origin_id
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "movies-oac"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+  origin_access_control_origin_type = "s3"
+}
 
-    # Configure as a custom origin
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+resource "aws_cloudfront_distribution" "CF_Distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = "myS3Origin"
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Terraform"
+  comment             = "Managed by Terraform"
   default_root_object = "index.html"
 
+  aliases = ["www.${var.domain_name}", "${var.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
+    target_origin_id = "myS3Origin"
 
     forwarded_values {
       query_string = false
@@ -129,21 +173,48 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
   }
 
   restrictions {
     geo_restriction {
       restriction_type = "none"
-      locations        = []
     }
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.cert_p10.arn
+    ssl_support_method  = "sni-only"
   }
+
+  depends_on = [aws_acm_certificate_validation.cert_validation_p10]
+}
+
+resource "aws_route53_record" "records_for_cf" {
+  zone_id = aws_route53_zone.zone_p10.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+
+  depends_on = [ aws_cloudfront_distribution.s3_distribution ]
+}
+
+resource "aws_route53_record" "records_for_cf_www" {
+  zone_id = aws_route53_zone.zone_p10.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+
+  depends_on = [ aws_cloudfront_distribution.s3_distribution ]
 }
 
 ###########
